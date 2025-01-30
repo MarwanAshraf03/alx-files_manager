1f73504b2ed72311347e9518ba6b1a23b6f6dc67
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2025-01-31 00:09:54 +0200

    1

--- utils/redis.js
+++ utils/redis.js
@@ -15,8 +15,14 @@ class RedisClient {
   }
 
   async get(key) {
-    const gett = promisfy(this.client.get).bind(this.client);
-    return await gett(key);
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, reply) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(reply);
+      });
+    });
   }
 
   async set(key, value, duration) {
