2840e5a0bad789156dfaed84cb2461219c989518
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2025-01-31 01:07:00 +0200

    1

--- utils/db.js
+++ utils/db.js
@@ -1,24 +1,27 @@
-import MongoClient from "mongodb/lib/mongo_client";
+import MongoClient from 'mongodb/lib/mongo_client';
 
 class DBClient {
   constructor() {
-    const port = process.env.DB_PORT ?? "27017";
-    const host = process.env.HOST ?? `mongodb://localhost:${port}`;
-    const database = process.env.DB_DATABASE ?? "files_manager";
+    const port = process.env.DB_PORT || '27017';
+    const host = process.env.HOST || `mongodb://localhost:${port}`;
+    const database = process.env.DB_DATABASE || 'files_manager';
     this.db = null;
     MongoClient.connect(host, { useUnifiedTopology: true }, (err, client) => {
       if (err) console.log(err);
       this.db = client.db(database);
     });
   }
+
   isAlive() {
     return this.db !== null;
   }
+
   async nbUsers() {
-    return this.db.collection("users").countDocuments();
+    return this.db.collection('users').countDocuments();
   }
+
   async nbFiles() {
-    return this.db.collection("files").countDocuments();
+    return this.db.collection('files').countDocuments();
   }
 }
 
