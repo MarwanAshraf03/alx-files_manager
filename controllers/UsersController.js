7c5689c3c82062bc8b37006c7fe46524f558ef81
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2025-01-31 03:06:35 +0200

    3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,7 @@
 import sha1 from "js-sha1";
 import dbClient from "../utils/db";
 
-export function postNew(req, res) {
+export async function postNew(req, res) {
   if (!req.body.email) {
     return res.status(400).send({ error: "Missing email" });
   }
@@ -12,7 +12,7 @@ export function postNew(req, res) {
     email: req.body.email,
     password: sha1(req.body.password),
   };
-  const userExists = dbClient.db
+  const userExists = await dbClient.db
     .collection("users")
     .findOne({ email: user.email });
   if (userExists) {
