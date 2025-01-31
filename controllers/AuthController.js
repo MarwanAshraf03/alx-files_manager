89bc5f2614fe3815d81e4a3ef83203e6438d6961
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2025-01-31 03:00:35 +0200

    3

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,1 +1,10 @@
-function connect(req, res) {}
+import dbClient from "../utils/db";
+// import base64
+
+function connect(req, res) {
+  const authHeader = req.headers.authorization;
+  const user = dbClient.db.collection("users").findOne({ email: user.email });
+  if (!authHeader) {
+    return res.status(401).send({ error: "Unauthorized" });
+  }
+}
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,26 +1,25 @@
-import sha1 from 'js-sha1';
-import dbClient from '../utils/db';
+import sha1 from "js-sha1";
+import dbClient from "../utils/db";
 
-function postNew(req, res) {
+export function postNew(req, res) {
   if (!req.body.email) {
-    return res.status(400).send({ error: 'Missing email' });
+    return res.status(400).send({ error: "Missing email" });
   }
   if (!req.body.password) {
-    return res.status(400).send({ error: 'Missing password' });
+    return res.status(400).send({ error: "Missing password" });
   }
   const user = {
     email: req.body.email,
     password: sha1(req.body.password),
   };
   const userExists = dbClient.db
-    .collection('users')
+    .collection("users")
     .findOne({ email: user.email });
   if (userExists) {
-    return res.status(400).send({ error: 'Already exist' });
+    return res.status(400).send({ error: "Already exist" });
   }
   //   dbClient.db.collection("users").insertOne(user);
-  const id = dbClient.db.collection('users').findOne({ email: user.email })._id;
+  const id = dbClient.db.collection("users").findOne({ email: user.email })._id;
 
   return res.json({ id, email: user.email });
 }
-export default postNew;
