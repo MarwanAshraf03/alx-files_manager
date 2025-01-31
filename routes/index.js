012f99ba9c79c24f17a3f65540f76eeab15fb013
MarwanAshraf03 <marawanashraf556.study@gmail.com>
2025-01-31 02:11:40 +0200

    3

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
-import express from "express";
-import * as AppController from "../controllers/AppController";
-import * as UsersController from "../controllers/UsersController";
+import express from 'express';
+import * as AppController from '../controllers/AppController';
+import * as UsersController from '../controllers/UsersController';
 // import UserController from "../controllers/user.js";
 
 const router = express.Router();
 
-router.get("/status", AppController.getStatus);
-router.get("/stats", AppController.getStats);
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
-// router.post("/users", UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
 export default router;
